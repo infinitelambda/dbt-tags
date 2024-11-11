a6933255b671c1ab508da43e68897eff42b437ee
bsuryachandra <40120313+bsuryachandra@users.noreply.github.com>
2024-11-11 09:19:45 +0530

    feat: add macro to apply tags to source columns in on-run-end hook (#16)
    * feat: add macro to apply tags to source columns in on-run-end hook
    
    * feat: addressed code review comments
    
    * feat: remove obsolete logic
    
    * feat: update logic to verify tags on source columns
    
    ---------
    
    Co-authored-by: bchandra <bchandra@phdata.io>

--- macros/apply-tags/apply_column_tags.sql
+++ macros/apply-tags/apply_column_tags.sql
@@ -11,29 +11,7 @@
   {% set tag_ns = dbt_tags.get_resource_ns() %}
   {% set tag_name_separator = var('dbt_tags__tag_name_separator','~') %}
   {% set query %}
-
-    {% for key, value in model.columns.items() -%}
-      {% if value.tags is defined -%}
-        {% for column_tag in value.tags if dbt_tags.is_allowed_tags(column_tag.split(tag_name_separator)[0]) %}
-
-          {%- set relation -%}
-            {{ model.database }}.{{ model.schema }}.{{ model.alias or model.name }}
-          {%- endset %}
-
-          alter table {{ relation }}
-            alter column {{ key }}
-            set tag {{ tag_ns }}.{{ column_tag.split(tag_name_separator)[0] }} =
-            {%- if tag_name_separator in column_tag -%}
-              '{{ column_tag.split(tag_name_separator)[1] }}';
-            {%- else -%}
-              '{{ key }}';
-            {%- endif %}
-          {{- log("dbt_tags.apply_column_tags - Set tag [" ~ tag_ns ~ "." ~ column_tag  ~ "] on column [" ~ relation ~ ":" ~ key ~ "]", info=True) -}}
-
-        {%- endfor %}
-      {%- endif %}
-    {%- endfor %}
-
+    {{dbt_tags.apply_column_tags_query(model)}}
   {% endset %}
 
   {{ return(query) }}
