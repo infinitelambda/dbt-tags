0a1ecdfb3f6c61c939b20fed17817605a7756ac9
Brian <49793984+bkimjin@users.noreply.github.com>
2024-11-17 17:37:55 -0800

    feat: use dbt_tags__opt_in_default_naming_config to disable env management (#20)

--- macros/utils/get_resource_ns.sql
+++ macros/utils/get_resource_ns.sql
@@ -3,7 +3,10 @@
 {%- endmacro %}
 
 {% macro default__get_resource_ns() %}
-
-  {{ return(generate_database_name(var("dbt_tags__database", target.database)) ~ "." ~ generate_schema_name(var("dbt_tags__schema", target.schema))) }}
+  {% if var("dbt_tags__opt_in_default_naming_config", true) %}
+    {{ return(generate_database_name(var("dbt_tags__database", target.database)) ~ "." ~ generate_schema_name(var("dbt_tags__schema", target.schema))) }}
+  {% else %}
+    {{ return(var("dbt_tags__database", target.database)) ~ "." ~ var("dbt_tags__schema", target.schema)) }}
+  {% endif %}
 
 {% endmacro %}
