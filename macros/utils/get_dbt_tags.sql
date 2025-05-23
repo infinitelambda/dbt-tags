529448830e4f0ffbcda6d766f36b557bbbd0d2ac
David Hong <david.hong@invitae.com>
2025-05-23 02:52:01 -0400

    fix: wrap graph usage with if exists.

--- macros/utils/get_dbt_tags.sql
+++ macros/utils/get_dbt_tags.sql
@@ -3,18 +3,19 @@
 {%- endmacro %}
 
 {% macro default__get_dbt_tags(with_value=False, debug=False) %}
-
+  
   {% set resource_types = var('dbt_tags__resource_types') %}
   {% set found_tags = [] %}
+  {% if execute %}
 
-  {% for relation in graph.nodes.values() if (relation.resource_type | lower) in resource_types %}
-    {% do found_tags.extend(dbt_tags.get_dbt_relation_tags(relation=relation, with_value=with_value, debug=debug)) %}
-  {% endfor %}
+    {% for relation in graph.nodes.values() if (relation.resource_type | lower) in resource_types %}
+      {% do found_tags.extend(dbt_tags.get_dbt_relation_tags(relation=relation, with_value=with_value, debug=debug)) %}
+    {% endfor %}
 
-  {% for relation in graph.sources.values() if (relation.resource_type | lower) in resource_types %}
-    {% do found_tags.extend(dbt_tags.get_dbt_relation_tags(relation=relation, with_value=with_value, debug=debug)) %}
-  {% endfor %}
+    {% for relation in graph.sources.values() if (relation.resource_type | lower) in resource_types %}
+      {% do found_tags.extend(dbt_tags.get_dbt_relation_tags(relation=relation, with_value=with_value, debug=debug)) %}
+    {% endfor %}
 
+  {% endif %}
   {{ return(found_tags) }}
-
 {% endmacro %}
