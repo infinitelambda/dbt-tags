358030ae1f6383ac4674b62ae81bdc89d968105f
Dat Nguyen <dat@infinitelambda.com>
2025-07-14 09:09:14 +0700

    chore: fix indent

--- macros/utils/get_dbt_tags.sql
+++ macros/utils/get_dbt_tags.sql
@@ -10,13 +10,13 @@
   {% set resource_types = var('dbt_tags__resource_types') %}
   {% set found_tags = [] %}
 
-    {% for relation in graph.nodes.values() if (relation.resource_type | lower) in resource_types %}
-      {% do found_tags.extend(dbt_tags.get_dbt_relation_tags(relation=relation, with_value=with_value, debug=debug)) %}
-    {% endfor %}
+  {% for relation in graph.nodes.values() if (relation.resource_type | lower) in resource_types %}
+    {% do found_tags.extend(dbt_tags.get_dbt_relation_tags(relation=relation, with_value=with_value, debug=debug)) %}
+  {% endfor %}
 
-    {% for relation in graph.sources.values() if (relation.resource_type | lower) in resource_types %}
-      {% do found_tags.extend(dbt_tags.get_dbt_relation_tags(relation=relation, with_value=with_value, debug=debug)) %}
-    {% endfor %}
+  {% for relation in graph.sources.values() if (relation.resource_type | lower) in resource_types %}
+    {% do found_tags.extend(dbt_tags.get_dbt_relation_tags(relation=relation, with_value=with_value, debug=debug)) %}
+  {% endfor %}
 
   {{ return(found_tags) }}
 {% endmacro %}
